2d9d6693aee1833592fabf699cf3beede41d053b
Klāvs Kalniņš <klavs.kalnins@beetrootlab.com>
2023-10-01 18:02:43 +0300

    updated to sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="w-full flex flex-col gap-1">
+    <a class="text-lg p-2 hover:text-white/75" href="https://www.linkedin.com/in/kl%C4%81vs-kalni%C5%86%C5%A1-877633207/">Linkedin</a>
+    <a class="text-lg p-2 hover:text-white/75" href="https://github.com/KlavsKalnins">Github</a>
+</div>
