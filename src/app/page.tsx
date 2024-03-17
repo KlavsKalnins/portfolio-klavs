883ab819cfbb230946e38cedeea91cd20901559f
Klavs Kalnins <klavs.kalnins@beetrootlab.com>
2024-03-17 11:47:15 +0200

    added name and linkedin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex flex-col gap-2">
         {/* <div className="fixed left-0 top-0 flex w-full justify-center gap-2 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <div>Klāvs Kalniņš</div>
         </div> */}
@@ -15,6 +15,8 @@ export default function Home() {
           <div>Lietotnes</div>
           <div>Lietotnes</div>
         </div> */}
+        <a className='text-xl'>Klāvs Kalniņš</a>
+        <a className='text-lg underline' href='https://www.linkedin.com/in/kl%C4%81vs-kalni%C5%86%C5%A1-877633207/'>Linkedin</a>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
