e0ddcd443f131915ea43d1b94ab5a7fadd518c73
Klavs Kalnins <klavs.kalnins@beetrootlab.com>
2024-10-05 18:57:37 +0300

    finished bday

--- src/app/bday/page.tsx
+++ src/app/bday/page.tsx
@@ -45,46 +45,64 @@ const shuffleArray = (array: string[]) => {
     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]; // Swap
   }
   console.log("shuffled");
-  console.log(
-    shuffledArray.map((prompt) => `"${prompt}"`).join(",\n")
-  );
+  console.log(shuffledArray.map((prompt) => `"${prompt}"`).join(",\n"));
   return shuffledArray;
 };
 
 const copyFromGoogleForum: string = `
-  <div class="PlcjTc"><div class="NC79P">I have</div><div class="NC79P">I will</div><div class="NC79P">I should</div></div>
-`
+<div class="PlcjTc"><div class="NC79P">I’ve smashed my head onto an apple 3 times</div><div class="NC79P">Man ir bijis haskijs</div><div class="NC79P">Es izdzertu 3 kafija brikastīs</div><div class="NC79P">i have crashed a car while drunk</div><div class="NC79P">I don't associate with Niggers</div></div>
+<div class="PlcjTc"><div class="NC79P">I’ve fucked in līvu akvaparks</div><div class="NC79P">Es nekad neesmu bijusi Igaunijā</div><div class="NC79P">Esmu lecis no 12m trampalīna</div><div class="NC79P">i swam in 6 degrees in Gauja</div><div class="NC79P">Im a night person</div></div>
+<div class="PlcjTc"><div class="NC79P">I don’t like cats</div><div class="NC79P">Es neesmu salauzusi nevienu kaulu</div><div class="NC79P">Esmu iesprūdis liftā </div><div class="NC79P">I have had gonorhea</div><div class="NC79P">I tried heroine </div></div>
+`;
 
 const promptsTruthsAndLies: string[] = [
-  "Add final here",
-  "Add final here2",
-  "Add final here3",
+  "Esmu lecis no 12m trampalīna",
+  "I don’t like cats",
+  "Im a night person",
+  "Es nekad neesmu bijusi Igaunijā",
+  "i have crashed a car while drunk",
+  "Man ir bijis haskijs",
+  "Man agrāk ļoti raustijās valoda",
+  "I tried heroine",
+  "i swam in 6 degrees in Gauja",
+  "Es izdzertu 3 kafija brikastīs",
+  "I’ve fucked in līvu akvaparks",
+  "Es neesmu salauzusi nevienu kaulu",
+  "I have had gonorhea",
+  "Es ienīstu androidus",
+  "I’ve smashed my head onto an apple 3 times",
+  "Es māku taisīt cocktails",
+  "Esmu piedalijies florbola sacensībās",
+  "Esmu iesprūdis liftā",
 ];
 
 export default function Page() {
-  const [promptsTruthsAndLies, setPromptsTruthsAndLies] = useState<string[]>([]);
+  // const [promptsTruthsAndLies, setPromptsTruthsAndLies] = useState<string[]>(promptsTruthsAndLies);
 
   const [cardIndex, setcardIndex] = useState(0);
   const [truthAndLiesEnabled, settruthAndLiesEnabled] = useState(false);
   const [truthAndLiesIndex, settruthAndLiesIndex] = useState(0);
 
-  useEffect(() => {
-    const tempDiv = document.createElement('div');
-    tempDiv.innerHTML = copyFromGoogleForum;
+  // useEffect(() => {
+  //   const tempDiv = document.createElement("div");
+  //   tempDiv.innerHTML = copyFromGoogleForum;
 
-    // Select all divs with class NC79P (ignore classes, rely on the structure)
-    const divs = tempDiv.querySelectorAll('div.NC79P');
+  //   // Select all divs with class NC79P (ignore classes, rely on the structure)
+  //   const divs = tempDiv.querySelectorAll("div.NC79P");
 
-    // Extract text content from each NC79P div
-    const extractedText = Array.from(divs).map(div => div.textContent || '');
+  //   // Extract text content from each NC79P div
+  //   const extractedText = Array.from(divs).map((div) => div.textContent || "");
 
-    // Update the state with the extracted text
-    console.log("From google docs")
-    console.log(extractedText)
-    shuffleArray(extractedText);
-    // Update the state with the extracted text
-    setPromptsTruthsAndLies(extractedText);
-  }, []);
+  //   // Update the state with the extracted text
+  //   console.log("From google docs");
+  //   console.log(extractedText);
+  //   shuffleArray(extractedText);
+  //   // Update the state with the extracted text
+  //   setPromptsTruthsAndLies(extractedText);
+  // }, []);
+
+  const currentDate = new Date();
+  const targetDate = new Date("2024-10-06T17:00:00");
 
   return (
     <main className="flex h-full flex-col items-center justify-between p-4 bg-white max-w-[600px]">
@@ -93,19 +111,32 @@ export default function Page() {
           <>
             <div className="relative w-full h-[100%] bg-black rounded-lg p-8 min-h-[70vh] flex flex-col justify-between">
               <div className="absolute top-4 left-4 w-10 h-10 bg-red-500 rotate-45"></div>
-              <div className="absolute top-4 left-4 w-10 h-10 flex justify-center items-center text-xl font-bold"><p>{truthAndLiesIndex}</p></div>
+              <div className="absolute top-4 left-4 w-10 h-10 flex justify-center items-center text-xl font-bold">
+                <p>{truthAndLiesIndex + 1}</p>
+              </div>
               <div className="absolute top-4 right-4 w-10 h-10 bg-red-500 rotate-45"></div>
-              <div className="absolute top-4 right-4 w-10 h-10 flex justify-center items-center text-xl font-bold"><p>{truthAndLiesIndex}</p></div>
+              <div className="absolute top-4 right-4 w-10 h-10 flex justify-center items-center text-xl font-bold">
+                <p>{truthAndLiesIndex + 1}</p>
+              </div>
               <div className="absolute bottom-4 left-4 w-10 h-10 bg-red-500 rotate-45"></div>
-              <div className="absolute bottom-4 left-4 w-10 h-10 flex justify-center items-center text-xl font-bold"><p>{truthAndLiesIndex}</p></div>
+              <div className="absolute bottom-4 left-4 w-10 h-10 flex justify-center items-center text-xl font-bold">
+                <p>{truthAndLiesIndex + 1}</p>
+              </div>
               <div className="absolute bottom-4 right-4 w-10 h-10 bg-red-500 rotate-45"></div>
-              <div className="absolute bottom-4 right-4 w-10 h-10 flex justify-center items-center text-xl font-bold"><p>{truthAndLiesIndex}</p></div>
+              <div className="absolute bottom-4 right-4 w-10 h-10 flex justify-center items-center text-xl font-bold">
+                <p>{truthAndLiesIndex + 1}</p>
+              </div>
 
               <div className="h-20"></div>
-              <p className="text-xl text-center">{promptsTruthsAndLies[truthAndLiesIndex]}</p>
-              <div className="w-full flex justify-center gap-4">
-                <div
-                  className="bg-red-900 p-4 rounded-3xl font-extrabold text-xl cursor-pointer"
+              <p className="text-xl text-center">
+                {promptsTruthsAndLies[truthAndLiesIndex]}
+              </p>
+              <div className="w-full flex justify-center gap-4 mb-16">
+                <button
+                  className={`bg-red-900 p-4 rounded-3xl font-extrabold text-xl cursor-pointer text-center ${
+                    truthAndLiesIndex <= 0 && "opacity-50"
+                  }`}
+                  disabled={truthAndLiesIndex <= 0}
                   onClick={() => {
                     settruthAndLiesIndex(
                       truthAndLiesIndex <= 0 ? 0 : truthAndLiesIndex - 1
@@ -113,9 +144,15 @@ export default function Page() {
                   }}
                 >
                   Prev Truth
-                </div>
-                <div
-                  className="bg-red-900 p-4 rounded-3xl font-extrabold text-xl cursor-pointer"
+                </button>
+                <button
+                  className={`bg-red-900 p-4 rounded-3xl font-extrabold text-xl cursor-pointer ${
+                    truthAndLiesIndex >= promptsTruthsAndLies.length - 1 &&
+                    "opacity-50"
+                  }`}
+                  disabled={
+                    truthAndLiesIndex >= promptsTruthsAndLies.length - 1
+                  }
                   onClick={() => {
                     settruthAndLiesIndex(
                       truthAndLiesIndex >= promptsTruthsAndLies.length - 1
@@ -125,7 +162,7 @@ export default function Page() {
                   }}
                 >
                   Next Truth
-                </div>
+                </button>
               </div>
             </div>
           </>
@@ -171,7 +208,7 @@ export default function Page() {
           </>
         )}
 
-        {cardIndex >= 3 && (
+        {cardIndex >= 3 && currentDate >= targetDate && (
           <div
             className="bg-red-900 p-4 rounded-3xl font-extrabold text-xl cursor-pointer"
             onClick={() => {
@@ -181,19 +218,20 @@ export default function Page() {
             {!truthAndLiesEnabled ? "Play Truths" : "Close Truths"}
           </div>
         )}
-
-        <div
-          className="bg-red-900 p-4 rounded-3xl font-extrabold text-xl cursor-pointer"
-          onClick={() => {
-            if (cardIndex + 1 >= profiles.length) {
-              setcardIndex(0);
-            } else {
-              setcardIndex(cardIndex + 1);
-            }
-          }}
-        >
-          {cardIndex >= profiles.length - 1 ? "back" : "continue"}
-        </div>
+        {!truthAndLiesEnabled && (
+          <div
+            className="bg-red-900 p-4 rounded-3xl font-extrabold text-xl cursor-pointer"
+            onClick={() => {
+              if (cardIndex + 1 >= profiles.length) {
+                setcardIndex(0);
+              } else {
+                setcardIndex(cardIndex + 1);
+              }
+            }}
+          >
+            {cardIndex >= profiles.length - 1 ? "back" : "continue"}
+          </div>
+        )}
       </div>
 
       {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div> */}
