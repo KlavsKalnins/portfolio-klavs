23310878625b407f0343dd63500b6ca261263a28
Klāvs Kalniņš <klavskalninss@gmail.com>
2025-08-20 20:30:59 +0300

    cover letter

--- src/app/coverletter/page.tsx
+++ src/app/coverletter/page.tsx
@@ -75,43 +75,21 @@ export default function CoverLetter() {
 
       <div className="" ref={paragraphRef}>
         <p>
-          To Hr department at {COMPANY},
-          <br></br>I am writing to express my interest in{" "}
-          {JOB_POSITION} at {COMPANY}. It is a great pleasure to apply for this
-          position as I am impressed by {COMPANY}'s reputation and I believe that
-          my skills and experience can be a strong fit for improving {COMPANY}'s
-          growth and success.
-          <br />
-          <br />
-          I have a college degree in Computer Programming as a Programming
-          Technician and have been working as a Software Engineer for the past 7
-          years. During this time, I have gained extensive experience in by
-          coding, testing, architecting and debugging websites, software, and
-          games. I have worked on a variety of projects, ranging from massive
-          multiplayer games like Zeeverse to large-scale enterprise Logistics
-          and Trucking systems for Priority1 a company in the US.
-          This year in my free time I have gathered a team of 4 to create a live service game that Im proud of and You can find more about it on my LinkedIn.
-          <br />
-          <br />
-          The main reason why I have decided to apply for this position is
-          because Im moving to Oslo to support my partner and I am looking for
-          a job that will not only allow me to relocate to Norway, but also to
-          work on-site along with well minded people who enjoy their craft as
-          well as has continuous growth in professional career.
-          <br />
-          <br />
-          It would be an honor to represent and become an employee at {COMPANY}.
-          I am eager to learn the code base as well as provide meaningful
-          contributions to the team. I appreciate you taking the time to look
-          over my application and documents. Do not hesitate to contact me if
-          there is any further information needed to be required. I look forward
-          to being in touch with you.
-          <br />
-          <br />
-          Best regards,
-          <br />
-          <br />
-          {ME}
+        To Hr department at {COMPANY},
+        <br></br>
+        I am writing to express my interest in {JOB_POSITION} at {COMPANY}. It is a great pleasure to apply for this position as I am impressed by {COMPANY}'s reputation and I believe that my skills and experience can be a strong fit for improving {COMPANY}'s growth and success.
+<br></br><br></br>
+        I have a college degree in Computer Programming as a Programming Technician and have been working as a Software Engineer for the past 7 years. During this time, I have gained extensive experience by coding, testing, architecting, and debugging websites, software, and games. I have worked on a variety of projects, ranging from massive multiplayer games like Zeeverse to large-scale enterprise Logistics and Trucking systems for Priority1, a company in the US. This year in my free time, I have gathered a team of 4 to create a live service game that I'm proud of, and you can find more about it on my LinkedIn.
+<br></br><br></br>
+        The main reason why I have decided to apply for this position is because I'm moving to Oslo to support my partner, and I am looking for a job that will not only allow me to relocate to Norway, but also to work on-site along with well-minded people who enjoy their craft as well as have continuous growth in professional career.
+<br></br><br></br>
+        It would be an honor to represent and become an employee at {COMPANY}. I am helpful and supportive of my colleagues, and I always engage with them and the workplace. I often participate in innovation and idea generation as well as contribute to sharing information amongst my group. These qualities have made me truly valuable in my team, and I believe they would be meaningful to {COMPANY}.
+<br></br><br></br>
+        I appreciate you taking the time to look over my application and documents. Do not hesitate to contact me if there is any further information is needed to be required. I look forward to being in touch with you.
+<br></br><br></br>
+        Best regards,<br></br>
+        Klāvs Kalniņš
+
         </p>
       </div>
 
